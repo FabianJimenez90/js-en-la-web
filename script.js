da177d7640ae4d5d7323de772867bf695d4252b7
David Jiménez <92261450+FabianJimenez90@users.noreply.github.com>
2023-02-26 12:14:08 -0500

    clase02 completada

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const  btn = document.querySelector(" [data-form-btn]");
+
+const crateTask = (evento)  => {
+    evento.preventDefault( );
+        const input = document.querySelector("[data-form-input]");
+        console.log(input.value);
+};
 console.log(btn);
 
-btn-addEventListener( "click", function() {
-    console.log("crear tarea")
-})
+btn.addEventListener(`click` , crateTask);
+// Arrow funciones anonimas
