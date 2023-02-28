4ffb0497bd1ba44be1dfe9bdac8299fd2746f0fa
David Jiménez <92261450+FabianJimenez90@users.noreply.github.com>
2023-02-28 07:40:21 -0500

    components

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-( ( ) => {
+import checkComplete from "./components/checkComplete.js";
+import deleteIcon from "./components/deleteIcon.js";
+
     const  btn = document.querySelector(" [data-form-btn]");
 
 const createTask = (evento)  => {
@@ -27,28 +29,3 @@ list.append(task );
 
 btn.addEventListener(`click` , createTask);
 
-const checkComplete = ( ) => {
-    const i = document.createElement(`i`);
-    i.classList.add(`far`, `fa-check-square`, `icon` ); 
-    i.addEventListener(`click`, completeTask);
-    return i;
-}
-
-const completeTask = ( event) => {
-    const element = event.target;
-    element.classList.toggle(`fas`);
-    element.classList.toggle(`completeIcon`);
-    element.classList.remove(`far`);
-}
-} ) ()
-
-const deleteIcon = ( ) => {
-const i = document.createElement(`i`);
-i.classList.add(`fas`, `fa-trash-alt`,`trashIcon`, `icon` );
-i.addEventListener("click",deleteTask) ;
-return i;
-}
-const deleteTask = ( event) => {
-     const parent = event.target.parentElement;
-     parent.remove( );
-}
